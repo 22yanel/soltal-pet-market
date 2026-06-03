a1b9891b3ae9fc26e0d3f89e4c91567be48968a5
22yanel <131114241+22yanel@users.noreply.github.com>
2026-06-03 11:53:16 -0400

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/mi-cuenta`,
+       redirectTo: "https://soltal-pet-market-wvtr.vercel.app/mi-cuenta",
       },
     });
 
